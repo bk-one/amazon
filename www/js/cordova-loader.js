caf94b284b68da93ed81b94c2d6e8e2c8420a6b3
Jannis Hermanns <jannis@gmail.com>
2013-06-18 21:35:55 +0200

    Add cordova ios/android autoloader

---
+++ www/js/cordova-loader.js
@@ -0,0 +1,7 @@
+function loadCordova() {
+  if (navigator.userAgent.indexOf("Android") > 0) {
+    $(document.createElement('script')).attr("src", "./cordova-android.js").appendTo("head");
+  } else if (navigator.userAgent.indexOf("iPhone") > 0 || navigator.userAgent.indexOf("iPad") > 0 || navigator.userAgent.indexOf("iPod") > 0) {
+    $(document.createElement('script')).attr("src", "./cordova-ios.js").appendTo("head");
+  }
+}
